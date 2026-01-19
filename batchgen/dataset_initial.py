7bcd206920ee45f9b21f036debd40dd78790a973
sciadopitys <31415786+sciadopitys@users.noreply.github.com>
2026-01-19 14:08:10 -0600

    Rename variable for clarity

--- batchgen/dataset_initial.py
+++ batchgen/dataset_initial.py
@@ -71,10 +71,10 @@ def create_batches():
                         res_t = torch.load('9_electron_density_pt_scaled/' + cur_res + '1.pt')
                         xlist1.append(res_t)
                     
-            new_xlist = torch.stack(xlist1)
+            new_ps = torch.stack(xlist1)
             
             xlist.append(new_x)
-            pslist.append(new_xlist)
+            pslist.append(new_ps)
 
             # load in ground truth electron density
             new_y = torch.load('electron_density_pt_scaled/' + trainlist[j] + '_fft.pt')
