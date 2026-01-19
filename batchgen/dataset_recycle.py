3c1c7c89de0506817482a9a31a3dbce3775de9a9
sciadopitys <31415786+sciadopitys@users.noreply.github.com>
2026-01-19 14:04:46 -0600

    Added comments for recycling batch generation script

--- batchgen/dataset_recycle.py
+++ batchgen/dataset_recycle.py
@@ -7,6 +7,8 @@ os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
 
 import random
+
+# shuffle indices within a specified range of a list
 def shuffle_slice(a, start, stop):
     i = start
     while (i < (stop-1)):
@@ -16,31 +18,34 @@ def shuffle_slice(a, start, stop):
 
 
 def create_batches():
-    
-    with open("example_ids/train_new_dataset2.txt") as myfile1: #select the first n_train examples as the training set, rest as validation set
+
+    # read in sorted list of training set example ids
+    with open("example_ids/train_new_dataset2.txt") as myfile1: 
         trainlist = myfile1.readlines()
     trainlist  = [x.rstrip() for x in trainlist]
     
-
-    with open("example_ids/size_indices_new1.txt") as myfile: #select the first n_train examples as the training set, rest as validation set
+    # read in list of indices separating examples of different tensor dimensions
+    with open("example_ids/size_indices_new1.txt") as myfile:
         sindices = myfile.readlines()
     sindices  = [x.rstrip() for x in sindices]
-    
+
+    # shuffle examples within tensor dimension bins
     for i in range(len(sindices) - 1):
         start = int(sindices[i])
         end = int(sindices[i+1])
         shuffle_slice(trainlist, start, end)
-        
-    with open("example_ids/training_indices_new2.txt") as myfile2: #select the first n_train examples as the training set, rest as validation set
+
+    # read in list of indices defining effective training batches
+    with open("example_ids/training_indices_new2.txt") as myfile2: 
         indices = myfile2.readlines()
     indices  = [x.rstrip() for x in indices]
 
-    
+    # read in list of primary sequences of training set examples
     with open("example_ids/new-dipeptide-AA-type-noclash_new2.list") as myfile1:
         examples = myfile1.readlines()
     examples  = [x.rstrip() for x in examples]
     
-
+    # create each training batch
     for i in range(len(indices) - 1):
         start = int(indices[i])
         end = int(indices[i+1])
@@ -48,16 +53,18 @@ def create_batches():
         pslist = []
         ylist = []
         for j in range(start, end):
-        
+
+            # load in Patterson map
             new_x = torch.load('patterson_pt_scaled/' + trainlist[j] + '_patterson.pt')
             new_x = torch.unsqueeze(new_x, 0)
 
-            
+            # load in previous model prediction (template map)
             new_x3 = torch.load('predictions/' + trainlist[j] + '.pt')
             new_x3 = new_x3[0,0,:,:,:]
             new_x3 = torch.unsqueeze(new_x3, 0)
             
-            
+
+            # search for current example's primary sequence and accumulate corresponding partial structure tensors
             for k in examples:
                 line = k.split(" ")
                 id = line[-1][:-4]
@@ -72,19 +79,23 @@ def create_batches():
                     
             new_xlist = torch.stack(xlist1)
             
-            
-            
+            # concatenate Patterson and template tensors
             new_xcomb = torch.cat((new_x, new_x3), 0)
+            
             xlist.append(new_xcomb)
-            #xlist.append(new_x)
             pslist.append(new_xlist)
+
+            # load in ground truth electron density
             new_y = torch.load('electron_density_pt_scaled/' + trainlist[j] + '_fft.pt')
             new_y = torch.unsqueeze(new_y, 0)
             ylist.append(new_y)
-        
+
+        # combine corresponding tensors
         data_x = torch.stack(xlist)
         data_ps = torch.stack(pslist)
         data_y = torch.stack(ylist)
+
+        # save tensors defining training batch
         torch.save(data_x, 'patterson_pt_scaled_newps/train_' + str(i) + '_patterson.pt')  
         torch.save(data_ps, 'patterson_pt_scaled_newps/train_' + str(i) + '_ps.pt')
         torch.save(data_y, 'electron_density_pt_scaled_newps/train_' + str(i) + '.pt')
