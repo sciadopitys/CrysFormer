d3a01181092436506712d30535219e9bfb4e231e
sciadopitys <31415786+sciadopitys@users.noreply.github.com>
2026-01-20 00:40:23 -0600

    Added comments to model definition (model/vit_3d_newps_dist.py)

--- model/vit_3d_newps_dist.py
+++ model/vit_3d_newps_dist.py
@@ -10,6 +10,7 @@ import model.nystrom_ps_rapid as nystrom_att
 def pair(t):
     return t if isinstance(t, tuple) else (t, t)
 
+# Layer normalization of current tokens before feedforward block
 class PreNorm(nn.Module):
     def __init__(self, dim, fn):
         super().__init__()
@@ -18,6 +19,7 @@ class PreNorm(nn.Module):
     def forward(self, x, **kwargs):
         return self.fn(self.norm(x), **kwargs)
 
+# Transformer feedforward block
 class FeedForward(nn.Module):
     def __init__(self, dim, hidden_dim, dropout = 0.):
         super().__init__()
@@ -31,6 +33,7 @@ class FeedForward(nn.Module):
     def forward(self, x):
         return self.net(x)
 
+# Layer normalization of current tokens and partial structure tokens before attention
 class PreNorm_partial_structure(nn.Module):
     def __init__(self, dim, fn):
         super().__init__()
@@ -40,32 +43,25 @@ class PreNorm_partial_structure(nn.Module):
     def forward(self, x, partial_structure, **kwargs):
         return self.fn(self.norm(x), self.norm_p(partial_structure), **kwargs)
 
-class FeedForward_partial_structure(nn.Module):
-    def __init__(self, dim, hidden_dim, dropout = 0.):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Linear(dim, hidden_dim),
-            nn.GELU(),
-            nn.Dropout(dropout),
-            nn.Linear(hidden_dim, dim),
-            nn.Dropout(dropout)
-        )
-    def forward(self, x):
-        return self.net(x)
-
 class Transformer_partial_structure(nn.Module):
     def __init__(self, patch_dim, num_patches, num_patches_ps, patch_height, patch_width, frame_patch_size, num_partial_structure, dim, depth, heads, dim_head, mlp_dim, same_partial_structure_emb=True,dropout = 0.):
         super().__init__()
+
+        # Positional encoding for partial structures
         self.pos_embedding = nn.Parameter(torch.randn(1, num_patches*num_partial_structure, dim))
+        
         self.layers = nn.ModuleList([])
         self.same_partial_structure_emb=same_partial_structure_emb
         if self.same_partial_structure_emb:
+            # Create transformer blocks of attention block followed by feedforward block
             for _ in range(depth):
                 self.layers.append(nn.ModuleList([
                     #PreNorm_partial_structure(dim, Attention_partial_structure(dim, heads = heads, dim_head = dim_head, dropout = dropout)),
                     PreNorm_partial_structure(dim, nystrom_att.Nystrom_attention_partial_structure(dim, heads = heads, dim_head = dim_head, num_landmarks = 64, residual = True, dropout = dropout)),
                     PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout))
                 ]))
+
+            # Single patch embedding for partial structures, used in all transformer layers
             self.partial_structure_to_patch_embedding = nn.Sequential(
                 Rearrange('b p c (f pf) (h p1) (w p2) -> b (p f h w) (c p1 p2 pf)', p1 = patch_height, p2 = patch_width, pf = frame_patch_size),
                 nn.LayerNorm(patch_dim),
@@ -81,6 +77,8 @@ class Transformer_partial_structure(nn.Module):
                 ]))
             self.partial_structure_rearrange=Rearrange('b p c (f pf) (h p1) (w p2) -> b (p f h w) (c p1 p2 pf)', p1 = patch_height, p2 = patch_width, pf = frame_patch_size)
             self.partial_structure_emb_layers=nn.ModuleList([])
+
+            # Provide a different partial structure token embedding to each transformer layer
             for _ in range(depth):
                 self.partial_structure_emb_layers.append(nn.Sequential(
                     nn.LayerNorm(patch_dim),
@@ -89,13 +87,15 @@ class Transformer_partial_structure(nn.Module):
                 ))
     def forward(self, x, partial_structure):
         if self.same_partial_structure_emb:
-            
+
+            # Create constant partial structure token embedding
             partial_structure = self.partial_structure_to_patch_embedding(partial_structure)
             _,total_partial_patches,_ = partial_structure.shape
             partial_structure = partial_structure + self.pos_embedding[:,:total_partial_patches,:]
             
             for attn, ff in self.layers:
-                
+
+                # Attention followed by feedforward
                 x = attn(x,partial_structure) + x    
                 x = ff(x) + x
                 
@@ -104,10 +104,15 @@ class Transformer_partial_structure(nn.Module):
             partial_structure = self.partial_structure_rearrange(partial_structure)
             _,total_partial_patches,_ = partial_structure.shape
             for i in range(len(self.layers)):
+
+                # Block-specific partial structure embeddiing
                 partial_structure_i=self.partial_structure_emb_layers[i](partial_structure)
                 partial_structure_i = partial_structure_i + self.pos_embedding[:,:total_partial_patches,:]
+
+                # Attention followed by feedforward
                 x = self.layers[i][0](x,partial_structure_i) + x
                 x = self.layers[i][1](x) + x
+                
             return x
 
 
@@ -327,4 +332,5 @@ class ViT_vary_encoder_decoder_partial_structure(nn.Module):
         x= x[:,:,pad_list[4]:(x.shape[2]-pad_list[5]),pad_list[2]:(x.shape[3]-pad_list[3]),pad_list[0]:(x.shape[4]-pad_list[1])]
         
         return(x)
+
 
